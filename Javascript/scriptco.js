48c03a79777c45d0fe2d2cd31fc40944ae64467b
KevinCarcel <kevcarcel@yahoo.fr>
2023-10-25 16:20:51 +0200

    commit 25/10

---
+++ Javascript/scriptco.js
@@ -0,0 +1,8 @@
+let formulaireConnexion = document.getElementById("formulaireConnexion");
+
+formulaireConnexion.addEventListener('submit2', function (e) {
+
+  let mailCo = document.getElementById("mailCo"); /*page de connexion */
+    let adressMail = localStorage.getItem('mail');/* index*/
+    console.log(adressMail);
+});
